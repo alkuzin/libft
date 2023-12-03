2df7479e21b037f8dd6f6aaa93b14d0745af654e
alkuzin <alkuzindev@gmail.com>
2023-12-03 12:08:11 +0300

    Added stdio/printf/ft_printf_int.c

--- libft/stdio/printf/ft_printf_int.c
+++ libft/stdio/printf/ft_printf_int.c
@@ -6,29 +6,14 @@
 /*   By: alkuzin <[null]@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 21:03:31 by alkuzin           #+#    #+#             */
-/*   Updated: 2023/12/01 21:11:55 by alkuzin          ###   ########.fr       */
+/*   Updated: 2023/12/03 11:34:26 by alkuzin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int ft_int_len(int n);
-
-static int ft_int_len(int n)
-{
-    int count;
-
-    while(n > 0)
-    {
-        n /= 10;
-        count++;
-    }
-
-    return count;
-}
-
 int ft_printf_int(int n)
 {
-    ft_putstr(ft_itoa(n));
-    return ft_int_len(n);
+    ft_putstr(ft_printf_itoa(n));
+    return ft_itoa_len(n);
 }
