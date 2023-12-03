b4adaf57b80452590053c3250d46f671979f2b65
alkuzin <alkuzindev@gmail.com>
2023-12-03 12:09:06 +0300

    Updated string/ft_itoa.c

--- libft/string/ft_itoa.c
+++ libft/string/ft_itoa.c
@@ -6,54 +6,63 @@
 /*   By: alkuzin <[null]@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 17:32:45 by alkuzin           #+#    #+#             */
-/*   Updated: 2023/11/29 19:24:13 by alkuzin          ###   ########.fr       */
+/*   Updated: 2023/12/03 11:45:45 by alkuzin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_string.h"
 
-char *ft_itoa(int n)
+size_t ft_itoa_len(long n)
 {
-    char *str;
-    int nb;
-    int i;
-    int j;
-    int k;
-    int is_negative;
-   
-    if(n == 0)
-    {
-        str = (char *)malloc(sizeof(char) * 2);
-        str[0] = '0';
-        str[1] = '\0';
-        return str;
-    } 
-    
-    is_negative = (n < 0);
-    nb = ABS(n);
-    i = 0;
-    while(nb > 0)
-    {
-        nb /= 10;
-        i++;
-    }
-    str = (char *)malloc(sizeof(char) * (i + 1 + is_negative));
+	size_t len;
 
-    if(!str)
-        return NULL;
+	len = 0;
+	if (n == 0)
+		return 1;
+	if (n < 0)
+	{
+		len++;
+		n = -n;
+	}
+	while (n >= 1)
+	{
+		len++;
+		n /= 10;
+	}
+	return len;
+}
 
-    if(is_negative)
-        str[0] = '-';
+static char	*ft_num_to_str(long n, char *str, size_t len)
+{
+	str = ft_calloc(len + 1, sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	if (n < 0)
+	{
+		str[0] = '-';
+		n = -n;
+	}
+	len--;
+	while (len)
+	{
+		str[len] = (n % 10) + '0';
+		n /= 10;
+		len--;
+	}
+	if (str[0] != '-')
+		str[0] = (n % 10) + '0';
+	return (str);
+}
 
-    nb = ABS(n);
-    j = 0;
-    while(j < i)
-    {
-        k = (nb % (unsigned int)ft_pow(10, j + 1)) / (unsigned int)ft_pow(10, j);
-        str[i - j + is_negative - 1] = k + '0';
-        j++;
-    }
-    str[j + is_negative] = '\0';
+char *ft_itoa(long n)
+{
+	size_t len;
+	char *str;
 
-    return str;
+	len = ft_itoa_len(n);
+	str = 0;
+	str = ft_num_to_str(n, str, len);
+	if (!str)
+		return NULL;
+	return str;
 }
