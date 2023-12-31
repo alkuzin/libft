e29f11018ae6a66943658d3abac5828712fd2482
alkuzin <alkuzindev@gmail.com>
2023-12-31 15:49:42 +0300

    Updated libft/math/ft_variations.c file

--- libft/math/ft_variations.c
+++ libft/math/ft_variations.c
@@ -6,14 +6,17 @@
 /*   By: alkuzin <[null]@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 13:34:48 by alkuzin           #+#    #+#             */
-/*   Updated: 2023/12/01 18:39:54 by alkuzin          ###   ########.fr       */
+/*   Updated: 2023/12/31 15:33:01 by alkuzin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_math.h"
 
-unsigned int ft_variations(unsigned int m, unsigned int n)
+unsigned long ft_variations(unsigned int m, unsigned int n)
 {
+    if (n == m)
+        return ft_factorial(n);
+
     if(n - m == 1)
         return n;
 
