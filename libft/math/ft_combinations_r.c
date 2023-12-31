dc8d01669367714ef454be3f2e93eafd412a21bf
alkuzin <alkuzindev@gmail.com>
2023-12-31 15:49:42 +0300

    Updated libft/math/ft_combinations_r.c file

--- libft/math/ft_combinations_r.c
+++ libft/math/ft_combinations_r.c
@@ -6,19 +6,19 @@
 /*   By: alkuzin <[null]@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 18:00:13 by alkuzin           #+#    #+#             */
-/*   Updated: 2023/12/01 18:01:54 by alkuzin          ###   ########.fr       */
+/*   Updated: 2023/12/31 15:32:00 by alkuzin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_math.h"
 
-unsigned int ft_combinations_r(unsigned int m, unsigned int n)
+unsigned long ft_combinations_r(unsigned int m, unsigned int n)
 {
     
     if(n - m == 1)
         return n;
 
-    if(m == 0 || n == m)
+    if(m == 0)
         return 1;
 
     return ft_combinations(m, (n + m) - 1);
