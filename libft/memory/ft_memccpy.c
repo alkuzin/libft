e50b3641286adf9ab270fd45d9d7499ca4f4ffda
alkuzin <alkuzindev@gmail.com>
2023-12-31 22:08:07 +0300

    Updated libft/memory/ft_memccpy.c file

--- libft/memory/ft_memccpy.c
+++ libft/memory/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: alkuzin <[null]@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 16:43:58 by alkuzin           #+#    #+#             */
-/*   Updated: 2023/11/16 18:49:58 by alkuzin          ###   ########.fr       */
+/*   Updated: 2023/12/31 22:07:22 by alkuzin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,23 @@ void *ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
     unsigned char *cdest;
     const unsigned char *csrc;
-    unsigned char cc;
+    unsigned char *target;
     size_t i;
+
 
     cdest = (unsigned char *)dest;
     csrc = (const unsigned char *)src;
-    cc = (unsigned char)c;
-    
+
+    if ((target = ft_memchr(src, c, n)) == NULL)
+        return NULL;
+
     i = 0;
-    while(i < n)
+    while(csrc[i])
     {
         cdest[i] = csrc[i];
 
-        if(cdest[i] == cc)
-            return (dest + i + 1);
+        if(&csrc[i] == target)
+            return dest;
         i++;
     }
 
