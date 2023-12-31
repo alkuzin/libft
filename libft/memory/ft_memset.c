9f79af3da0f3e62cde41658dfa0bd87b12ed5141
alkuzin <alkuzindev@gmail.com>
2023-12-31 22:08:07 +0300

    Updated libft/memory/ft_memset.c file

--- libft/memory/ft_memset.c
+++ libft/memory/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: alkuzin <[null]@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 15:48:55 by alkuzin           #+#    #+#             */
-/*   Updated: 2023/11/18 16:16:28 by alkuzin          ###   ########.fr       */
+/*   Updated: 2023/12/31 22:07:37 by alkuzin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void *ft_memset(void *s, int c, size_t n)
     cc = (unsigned char)c;
     
     i = 0;
-    while(n--)
+    while(n-- && src[i])
     {
         src[i] = cc;
         i++;
