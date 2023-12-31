48e966775ff9ed1be192f9955de99f6fe478a8df
alkuzin <alkuzindev@gmail.com>
2023-12-31 15:49:42 +0300

    Updated libft/math/ft_variations_r.c file

--- libft/math/ft_variations_r.c
+++ libft/math/ft_variations_r.c
@@ -6,13 +6,34 @@
 /*   By: alkuzin <[null]@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 19:13:42 by alkuzin           #+#    #+#             */
-/*   Updated: 2023/11/29 19:17:24 by alkuzin          ###   ########.fr       */
+/*   Updated: 2023/12/31 15:33:07 by alkuzin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_math.h"
 
-unsigned int ft_variations_r(unsigned int m, unsigned int n)
+unsigned long ft_variations_r(unsigned int n, unsigned int m)
 {
-    return (unsigned int)ft_pow(n, m);
+    unsigned long result;
+    unsigned int i;
+
+
+    if(m == 0)
+        return 1;
+
+    if(n == 0)
+        return 0;
+
+    if (m >= n && ((m - n) >= 64))
+        return _INF;
+    
+
+    result = 1;
+    i = 0;
+    while(i < m)
+    {
+        result *= n;
+        i++; 
+    }
+    return result;
 }
