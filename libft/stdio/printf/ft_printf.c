be0c7a32c4b17a93be245c5d56adec7fb591c978
alkuzin <alkuzindev@gmail.com>
2024-01-11 17:02:18 +0300

    Updated libft/stdio/printf/ft_printf.c file

--- libft/stdio/printf/ft_printf.c
+++ libft/stdio/printf/ft_printf.c
@@ -6,13 +6,11 @@
 /*   By: alkuzin <[null]@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 18:45:27 by alkuzin           #+#    #+#             */
-/*   Updated: 2023/12/03 11:56:49 by alkuzin          ###   ########.fr       */
+/*   Updated: 2024/01/11 16:18:08 by alkuzin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-#include <stdio.h>
 
 /*
 static int ft_parse(const char* str, va_list args);
@@ -21,25 +19,29 @@ static int ft_parse_flags(const char* str, va_list args);
 
 static int ft_print_args(char type, va_list args);
 */
+
 static int ft_print_args(char type, va_list args)
 {
     int count;
 
+
     count = 0;
-    if(type == '%')
-    {
+    if(type == '%') {
         ft_putchar(type);
         count++;
     }
     else if(type == 'd' || type == 'i')
-        count += ft_printf_int((va_arg(args, int)));
+        count += ft_printf_int(va_arg(args, int));
+    else if(type == 'u')
+        count += ft_printf_uint(va_arg(args, unsigned int));
+    
+	else if(type == 'x' || type == 'X')
+        count += ft_printf_hex(va_arg(args, unsigned int), ft_isupper(type));
     
     else if(type == 's')
-        count += ft_printf_str((va_arg(args, char *)));
-    else if(type == 's')
-        count += ft_printf_str((va_arg(args, char *)));
+        count += ft_printf_str(va_arg(args, char *));
     else if(type == 'c')
-        count += ft_printf_char((va_arg(args, int)));
+        count += ft_printf_char(va_arg(args, int));
     return count;
 }
 
@@ -48,12 +50,10 @@ int ft_parse(const char* str, va_list args)
     int count;
     int i;
 
+
     i = -1;
-    while(str[++i]) 
-    {
-        if(str[i] == '%' && str[i + 1] != '\0')
-        {
-           // printf("str[i + 1]: '%c'\n", str[i+1]);
+    while(str[++i]) {
+        if(str[i] == '%' && str[i + 1] != '\0') {
             count += ft_print_args(str[i + 1], args); /* change to ft_parse_flags later */ 
             i++;
         }
@@ -70,20 +70,19 @@ int ft_printf(const char *format, ...)
     char *str;
     int count;
 
+
     if(!format || *format == '\0')
         return 0;
-
 
     str = ft_strdup(format);
     if(!str || *str == '\0')
         return 0;
 
     count = 0;
-    va_start(args, format);
-    
+    va_start(args, format); 
     count = ft_parse(str, args);
-
     va_end(args);
+
     free(str);
     return count;    
 }
