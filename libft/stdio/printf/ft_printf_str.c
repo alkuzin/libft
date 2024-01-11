d3303a3dbbc5bf796bf42ecc4f6ada2eb3c2003f
alkuzin <alkuzindev@gmail.com>
2024-01-11 17:02:18 +0300

    Updated libft/stdio/printf/ft_printf_str.c file

--- libft/stdio/printf/ft_printf_str.c
+++ libft/stdio/printf/ft_printf_str.c
@@ -6,7 +6,7 @@
 /*   By: alkuzin <[null]@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 10:55:16 by alkuzin           #+#    #+#             */
-/*   Updated: 2023/12/03 12:02:18 by alkuzin          ###   ########.fr       */
+/*   Updated: 2024/01/11 14:55:35 by alkuzin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,11 @@
 
 int ft_printf_str(char *str)
 {
-    if(!str)
-        return ft_putstr("(null)");
-    return ft_putstr(str);
+    if(!str) {
+        ft_putstr(PRINT_NULL);
+		return 0;
+	}
+    
+	ft_putstr(str);
+	return ft_strlen(str);
 }
